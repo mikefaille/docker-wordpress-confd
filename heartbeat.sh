d102d359fc3e605eb31f2195c3c0011a97cd54af
Michael <michael@faille.io>
2015-01-12 06:06:44 -0500

    Correct heartbeat.sh script

--- heartbeat.sh
+++ heartbeat.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 while true; do
-    etcdctl get /web/`hostname`/ip `ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1` --ttl '8'
+    etcdctl set  --ttl '8' /web/`hostname`/ip `ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
     sleep 2
 done
 
 
+#echo "    ssh -p <port> root@" $(etcdctl set  --ttl '8' /web/`hostname`/ip `ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`)
